a305d029e932d639e16ac1cdb74c636029c24d7e
RedAzaki <107935485+RedAzaki@users.noreply.github.com>
2023-01-28 15:48:31 +0500

    new file

---
+++ cypress/e2e/API/test_Api_GET.cy.js
@@ -0,0 +1,71 @@
+/*describe("Test api GET", () => {
+    it('GET-test', () => {
+        cy.request('GET', 'https://swapi.dev/api/people/1/').then((responce) =>{
+            expect(responce).to.have.property('status', 200);
+            expect(responce.body).to.not.be.null; 
+            expect(responce.body.films).to.have.length(4); 
+        })
+
+    })
+})*/
+
+describe('Test API', ()=>{
+    let accessToken = "fc429ea469eab87eaa9bc3744e1eb1f265b9a81362112eef76ec0c730e1b8ac7"
+    let randomText = ""
+    let testEmail = ""
+    let testName = ""
+    it('create user test', ()=>{
+
+        var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        for (var i=0; i < 5; i++)
+        randomText+=pattern.charAt(Math.floor(Math.random() * pattern.length));
+        testEmail = randomText + '@gmail.com'
+        
+        cy.fixture('createuser').then((payload) =>{      
+          
+            //1. create user (POST)
+            cy.request({
+                method : 'POST',
+                url : 'https://gorest.co.in/public/v2/users',
+                headers : {
+                    'Authorization' : "Bearer " +accessToken
+                },
+
+                body: {
+                    "name": payload.name,
+                    "gender": payload.gender,
+                    "email": `${testEmail}`,
+                    "status": payload.status,
+                },
+            }).then((response) =>{
+                expect(response.status).to.eq(201);
+                expect(response.body).to.have.property('name', payload.name);
+                expect(response.body).to.have.property('gender',payload.gender);
+                expect(response.body).to.have.property('email',`${testEmail}`);           
+                expect(response.body).to.have.property('status',payload.status);
+            }).then((res)=>{                
+              
+                const userId = res.body.id
+            
+                cy.log("user id is:" + userId)
+                
+                //2. get user (GET)
+                cy.request({
+                    method : 'GET',
+                    url : 'https://gorest.co.in/public/v2/users/'+ userId,
+                    headers : {
+                        'Authorization' : "Bearer " +accessToken
+                    }
+                })
+            }).then((res2) =>{
+                const userId = res2.body.id
+                cy.log("user id is:" + userId)
+                expect(res2.status).to.eq(200)
+                expect(res2.body).to.have.property('id',userId)
+                expect(res2.body).to.have.property('name',payload.name)
+                expect(res2.body).to.have.property('status',payload.status)
+                expect(res2.body).to.have.property('email',`${testEmail}`)
+            })
+        })
+    })
+})
